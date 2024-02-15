3f57bab6eb4e5e0e397e108538bc840f4ed1a366
Phretik <thomasalmond_123@hotmail.com>
2024-02-15 11:58:36 +0000

    Added tests

--- hello/Tests/tests_models.py
+++ hello/Tests/tests_models.py
@@ -1,4 +1,22 @@
 from django.test import TestCase
+from django.utils import timezone
+from hello.models import LogMessage
+import datetime
 
-# Create your tests here.
-#render
+class LogMessageTests(TestCase):
+
+    def create_logmessage(self):
+        """"
+        Create a log message instance
+        """""
+        return LogMessage.objects.create(
+            message="This is a test message.",
+            Log_date=timezone.now()
+        )
+
+    def test_logmessage_creation(self):
+        """"
+        Test the creation of the log message instance.
+        """""
+        log_message = self.create_logmessage()
+        self.assertTrue(isinstance(log_message, log_message))
